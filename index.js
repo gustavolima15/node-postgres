4a7c243de3be232fe2e97880cce695cdb67cbdbb
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-06-12 01:05:43 -0300

    feat: conexão com banco de dados usando o Client

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const { Client } = require('pg');
+
+
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    return res.json('Tudo certo')
+app.get('/', async (req, res) =>{
+    const client = new Client({
+        host: 'localhost',
+        port: 5432,
+        user: 'postgres',
+        password: 'postgres',
+        database: 'aula_conexao_node_pg'
+    });
+
+    try {
+        await client.connect();
+        const resultado = await client.query('select * from empresas');
+
+        await client.end();
+
+        return res.json(resultado.rowCount);
+    } catch (error) {
+        console.log(error.message)
+    }
 });
 
 app.listen(3000);
