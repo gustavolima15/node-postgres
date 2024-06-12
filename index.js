c1d322ff16ef270c9d2bc06645446988964e9387
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-06-12 01:12:34 -0300

    refactor: conexão utilizando o Pool

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require('express');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
 
+const poll = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'aula_conexao_node_pg'
+});
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', async (req, res) =>{
-    const client = new Client({
-        host: 'localhost',
-        port: 5432,
-        user: 'postgres',
-        password: 'postgres',
-        database: 'aula_conexao_node_pg'
-    });
 
     try {
-        await client.connect();
-        const resultado = await client.query('select * from empresas');
+       
+        const resultado = await poll.query('select * from empresas');
 
-        await client.end();
+       
 
         return res.json(resultado.rowCount);
     } catch (error) {
