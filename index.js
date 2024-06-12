e7d8bf822111c019c7489010747cce3b76f50528
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-06-12 01:31:24 -0300

    feat: passar valores dinâmicos na query

--- index.js
+++ index.js
@@ -14,15 +14,16 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', async (req, res) =>{
-
+app.get('/:id', async (req, res) =>{
+    const { id } = req.params;
     try {
-       
-        const resultado = await poll.query('select * from empresas');
+        // const query = 'select * from empresas where nome = $1 or nome = $2'
+        // const params = ['Google', 'Facebook']
+        const query = 'update empresas set site = $1 where id = $2'
+        const params = ['www.cakewalk.com', 1]
 
-       
-
-        return res.json(resultado.rowCount);
+        const resultado = await poll.query(query, params);
+        return res.json(resultado.rows);
     } catch (error) {
         console.log(error.message)
     }
